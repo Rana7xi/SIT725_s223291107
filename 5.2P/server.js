aa82e1f0425c3978ce058b7386e717927b89e734
Rana <rana7xi@gmail.com>
2025-12-10 12:53:36 +1100

    completed book catalog MVC

---
+++ 5.2P/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const booksRoutes = require('./routes/books');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(booksRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
